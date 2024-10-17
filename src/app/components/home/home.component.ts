8fe1cfba59257386e23a741630f94234dd23a26c
dohamoh <doh.mohammed@gmail.com>
2024-10-17 19:02:27 +0300

    homeImgs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,51 +14,51 @@ export class HomeComponent {
   selectedTab = 'tab1'; // Default to Tab 1
 
   images: string[] = [
-    '../../../assets/images/product/product-1.jpg',
-    '../../../assets/images/product/product-2.jpg',
-    '../../../assets/images/product/product-3.jpg',
-    '../../../assets/images/product/product-4.jpg',
-    '../../../assets/images/product/product-5.jpg',
-    '../../../assets/images/product/product-6.jpg',
-    '../../../assets/images/product/product-7.jpg',
-    '../../../assets/images/product/product-8.jpg',
+    '/assets/images/product/product-1.jpg',
+    '/assets/images/product/product-2.jpg',
+    '/assets/images/product/product-3.jpg',
+    '/assets/images/product/product-4.jpg',
+    '/assets/images/product/product-5.jpg',
+    '/assets/images/product/product-6.jpg',
+    '/assets/images/product/product-7.jpg',
+    '/assets/images/product/product-8.jpg',
   ]
   wImages: string[] = [
-    '../../../assets/images/product/product-1.jpg',
-    '../../../assets/images/product/product-3.jpg',
-    '../../../assets/images/product/product-6.jpg',
-    '../../../assets/images/product/product-7.jpg',
+    '/assets/images/product/product-1.jpg',
+    '/assets/images/product/product-3.jpg',
+    '/assets/images/product/product-6.jpg',
+    '/assets/images/product/product-7.jpg',
   ]
   mImages: string[] = [
-    '../../../assets/images/product/product-2.jpg',
-    '../../../assets/images/product/product-4.jpg',
-    '../../../assets/images/product/product-5.jpg',
-    '../../../assets/images/product/product-8.jpg',
+    '/assets/images/product/product-2.jpg',
+    '/assets/images/product/product-4.jpg',
+    '/assets/images/product/product-5.jpg',
+    '/assets/images/product/product-8.jpg',
   ]
   pictures: string[] = [
-    '../../../assets/images/insta/insta-1.jpg',
-    '../../../assets/images/insta/insta-2.jpg',
-    '../../../assets/images/insta/insta-4.jpg',
-    '../../../assets/images/insta/insta-5.jpg',
-    '../../../assets/images/insta/insta-6.jpg',
+    '/assets/images/insta/insta-1.jpg',
+    '/assets/images/insta/insta-2.jpg',
+    '/assets/images/insta/insta-4.jpg',
+    '/assets/images/insta/insta-5.jpg',
+    '/assets/images/insta/insta-6.jpg',
   ]
   acsImages: string[] = [
-    '../../../assets/images/product/product-9.jpg',
-    '../../../assets/images/product/product-4 (2).jpg',
-    '../../../assets/images/product/product-5 (2).jpg',
-    '../../../assets/images/product/product-7 (2).jpg',
+    '/assets/images/product/product-9.jpg',
+    '/assets/images/product/product-4 (2).jpg',
+    '/assets/images/product/product-5 (2).jpg',
+    '/assets/images/product/product-7 (2).jpg',
   ]
   kidsImages: string[] = [
-    '../../../assets/images/product/kids-14.webp',
-    '../../../assets/images/product/kids-5.webp',
-    '../../../assets/images/product/kids-8.jpg',
-    '../../../assets/images/product/kids-9.webp',
+    '/assets/images/product/kids-14.webp',
+    '/assets/images/product/kids-5.webp',
+    '/assets/images/product/kids-8.jpg',
+    '/assets/images/product/kids-9.webp',
   ]
   cosImages: string[] = [
-    '../../../assets/images/product/cosmatic-3.webp',
-    '../../../assets/images/product/cosmatic-4.avif',
-    '../../../assets/images/product/cosmatic-2.webp',
-    '../../../assets/images/product/cosmatic-10.webp',
+    '/assets/images/product/cosmatic-3.webp',
+    '/assets/images/product/cosmatic-4.avif',
+    '/assets/images/product/cosmatic-2.webp',
+    '/assets/images/product/cosmatic-10.webp',
   ]
 
 
